f5550605817a6c5c07cc85096ea2c5fcc5866884
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2025-10-19 22:19:03 +0530

    Create dishaarrtools.h

---
+++ dishaarrtools.h
@@ -0,0 +1,23 @@
+// Filename: disha_arraytools.h
+#ifndef DISHA_ARRAYTOOLS_H
+#define DISHA_ARRAYTOOLS_H
+
+// Prints all elements of the array
+void printArray(int arr[], int n);
+
+// Reverses the array elements
+void reverseElements(int arr[], int n);
+
+// Returns the index of the largest element
+int indexOfMax(int arr[], int n);
+
+// Returns the index of the smallest element
+int indexOfMin(int arr[], int n);
+
+// Calculates and returns the average of array elements
+float averageOfArray(int arr[], int n);
+
+// Searches for a value using linear search and returns its index
+int searchInArray(int arr[], int n, int key);
+
+#endif
