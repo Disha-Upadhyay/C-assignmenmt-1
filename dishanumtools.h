1851de0740d990194b59e52cdc937ebe47c77b2a
Disha-Upadhyay <dishaupadhyay2007@gmail.com>
2025-10-19 22:22:52 +0530

    Create dishanumtools.h

---
+++ dishanumtools.h
@@ -0,0 +1,20 @@
+// Filename: dishanumtools.h
+#ifndef DISHANUMTOOLS_H
+#define DISHANUMTOOLS_H
+
+// Reverses the digits of a number
+int reverseNumber(int num);
+
+// Checks if the number is Armstrong
+int checkArmstrong(int num);
+
+// Checks if the number is Adam's number
+int checkAdams(int num);
+
+// Checks if the number is prime
+int isNumberPrime(int num);
+
+// Checks if the number is both prime and palindrome
+int isPrimeAndPalindrome(int num);
+
+#endif
